bfbf7752e5a5489a1f11fd55b008698115624869
Christian Frech <christianfrech00@gmail.com>
2024-02-28 11:07:51 -0800

    further modifications to add_move()

--- kmc_cpp/kmc_stripping_parallel.hpp
+++ kmc_cpp/kmc_stripping_parallel.hpp
@@ -576,16 +576,31 @@ class Lattice {
                 num_of_vacs = num_vacancies;
             }
 
-        void add_move(int i, int j, int k, int l, int curr_move_num, int s, int idx) {
-            moves_coords[curr_move_num][0] = i;
-            moves_coords[curr_move_num][1] = (((j - diag_directions[s][0]) % lattice_dim[0] + lattice_dim[0]) % lattice_dim[0]);
-            moves_coords[curr_move_num][2] = (((k - diag_directions[s][1]) % lattice_dim[1] + lattice_dim[1]) % lattice_dim[1]);
-            moves_coords[curr_move_num][3] = (((l - diag_directions[s][2]) % lattice_dim[2] + lattice_dim[2]) % lattice_dim[2]);                                        
-            moves_shifts[curr_move_num][0] = - diag_directions[s][0];
-            moves_shifts[curr_move_num][1] = - diag_directions[s][1];
-            moves_shifts[curr_move_num][2] = - diag_directions[s][2]; 
-            moves_lattice[curr_move_num][0] = 0;
-            moves_vacs[curr_move_num][0] = idx;
+        void add_move(int i, int j, int k, int l, int curr_move_num, int direc_sign, int s, int idx, int lattice) {
+            if ((lattice == 2) || (lattice == 3)) {
+                moves_coords[curr_move_num][0] = i;
+                moves_coords[curr_move_num][1] = (((j + edge_directions[s][0]) % lattice_dim[0] + lattice_dim[0]) % lattice_dim[0]);
+                moves_coords[curr_move_num][2] = (((k + edge_directions[s][1]) % lattice_dim[1] + lattice_dim[1]) % lattice_dim[1]);
+                moves_coords[curr_move_num][3] = (((l + edge_directions[s][2]) % lattice_dim[2] + lattice_dim[2]) % lattice_dim[2]);                                        
+                moves_shifts[curr_move_num][0] = edge_directions[s][0];
+                moves_shifts[curr_move_num][1] = edge_directions[s][1];
+                moves_shifts[curr_move_num][2] = edge_directions[s][2]; 
+                moves_lattice[curr_move_num][0] = lattice;
+                moves_vacs[curr_move_num][0] = idx;
+
+            }
+            else if ((lattice == 0) || (lattice == 1)) {
+                moves_coords[curr_move_num][0] = i;
+                moves_coords[curr_move_num][1] = (((j + direc_sign * diag_directions[s][0]) % lattice_dim[0] + lattice_dim[0]) % lattice_dim[0]);
+                moves_coords[curr_move_num][2] = (((k + direc_sign * diag_directions[s][1]) % lattice_dim[1] + lattice_dim[1]) % lattice_dim[1]);
+                moves_coords[curr_move_num][3] = (((l + direc_sign * diag_directions[s][2]) % lattice_dim[2] + lattice_dim[2]) % lattice_dim[2]);                                        
+                moves_shifts[curr_move_num][0] = direc_sign * diag_directions[s][0];
+                moves_shifts[curr_move_num][1] = direc_sign * diag_directions[s][1];
+                moves_shifts[curr_move_num][2] = direc_sign * diag_directions[s][2]; 
+                moves_lattice[curr_move_num][0] = lattice;
+                moves_vacs[curr_move_num][0] = idx;
+
+            }
         }
 
         void parallel_get_actions() {
@@ -625,14 +640,14 @@ class Lattice {
                     if ((i == 0) && (moves_shifts[idx][0] == 0)) {
                         if (j == 0) {
                             /*check neighbor -x,-y array */
-                            if !(proc_x_neighbors[0][0]) {add_move(i,j,k,l,curr_move_num,s,idx);}
+                            if !(proc_x_neighbors[0][0]) {add_move(i,j,k,l,curr_move_num,-1,s,idx,0);}
                         }
                         else if (j == (chunk_bounds[1] - 1)) {
                             /*check neighbor -x,+y array */
-                            if !(proc_x_neighbors[1][0]) {add_move(i,j,k,l,curr_move_num,s,idx);}
+                            if !(proc_x_neighbors[1][0]) {add_move(i,j,k,l,curr_move_num,-1,s,idx,0);}
                         }
                         else {
-                            if !(y_proc_neighbors[0][(j+1)]) {add_move(i,j,k,l,curr_move_num,s,idx);}
+                            if !(y_proc_neighbors[0][(j+1)]) {add_move(i,j,k,l,curr_move_num,-1,s,idx,0);}
                         }
                     }
                     
